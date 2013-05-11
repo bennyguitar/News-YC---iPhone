3527d0b8f667b0120df53ef99e1642357d8ecc2b
BenGordon <brgordon@ua.edu>
2013-05-11 15:09:03 -0500

    Basics added for submitting a link

---
+++ HackerNews/SubmitLinkViewController.h
@@ -0,0 +1,13 @@
+//
+//  SubmitLinkViewController.h
+//  HackerNews
+//
+//  Created by Benjamin Gordon on 5/11/13.
+//  Copyright (c) 2013 Benjamin Gordon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SubmitLinkViewController : UIViewController
+
+@end
---
+++ HackerNews/SubmitLinkViewController.m
@@ -0,0 +1,38 @@
+//
+//  SubmitLinkViewController.m
+//  HackerNews
+//
+//  Created by Benjamin Gordon on 5/11/13.
+//  Copyright (c) 2013 Benjamin Gordon. All rights reserved.
+//
+
+#import "SubmitLinkViewController.h"
+
+@interface SubmitLinkViewController ()
+
+@end
+
+@implementation SubmitLinkViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
