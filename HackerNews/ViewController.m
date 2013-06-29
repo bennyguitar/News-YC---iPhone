ab09286bb97d949c19d941207ff4b892b0d60112
Benjamin Gordon <brgordon@ua.edu>
2013-06-29 11:34:25 -0500

    v1.2.3 release to Apple!

--- HackerNews/ViewController.m
+++ HackerNews/ViewController.m
@@ -430,6 +430,7 @@
             
             // Color cell elements
             cell.titleLabel.textColor = [[HNSingleton sharedHNSingleton].themeDict objectForKey:@"MainFont"];
+            cell.titleLabel.backgroundColor = tableView.backgroundColor;
             cell.postedTimeLabel.textColor = [[HNSingleton sharedHNSingleton].themeDict objectForKey:@"SubFont"];
             cell.postedTimeLabel.backgroundColor = [[HNSingleton sharedHNSingleton].themeDict objectForKey:@"BottomBar"];
             cell.scoreLabel.textColor = [[HNSingleton sharedHNSingleton].themeDict objectForKey:@"SubFont"];
@@ -459,6 +460,7 @@
                 if ([[cell.titleLabel.text substringWithRange:NSMakeRange(0, 9)] isEqualToString:@"Show HN: "]) {
                     UIView *showHNView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, cell.frame.size.width, cell.frame.size.height)];
                     showHNView.backgroundColor = [[HNSingleton sharedHNSingleton].themeDict objectForKey:@"ShowHN"];
+                    cell.titleLabel.backgroundColor = [[HNSingleton sharedHNSingleton].themeDict objectForKey:@"ShowHN"];
                     [cell insertSubview:showHNView atIndex:0];
                 }
             }
