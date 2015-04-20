21962f99681d7b26726156bc2ce2430346cad94e
Ben Gordon <bgordon@curse.com>
2015-04-20 13:05:05 -0500

    Updated Pods, and using the newest libHN

--- HN/AppDelegate.swift
+++ HN/AppDelegate.swift
@@ -51,6 +51,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        HNManager.sharedManager().downloadAndSetConfiguration();
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
